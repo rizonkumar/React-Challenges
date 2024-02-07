a9c9f8599a073539571ec2c47d6bc4914f16d396
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-02-07 11:16:29 +0530

    Completed Challenge - 3

--- challenge-3/src/App.js
+++ challenge-3/src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import Challenge3 from "./Challenge3";
 
 function App() {
+    return (
+        <div>
+            <h3>Challenge - 3</h3>
+            <p>Conditional Rendering Puzzle: Build a react component that renders "Hello World" initally. After "5" sec
+                change the text to "Goodbye!" without using any external state management libaries / timer
+            </p>
 
+            <p>Updated the question with added the timer as well and once the message is changed stop the timer</p>
+            <Challenge3 />
+        </div>
+    )
 }
 
 export default App;
---
+++ challenge-3/src/Challenge3.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+const Challenge3 = () => {
+  const [message, setMessage] = useState("Hello World");
+  const [timerValue, setTimerValue] = useState(0);
+  const [messageChanged, setMessageChanged] = useState(false);
+
+  useEffect(() => {
+    const interval = 1000; // Set the interval for updating the timer in milliseconds
+
+    const timer = setTimeout(() => {
+      setMessage("GoodBye!");
+      setMessageChanged(true);
+    }, 5000);
+
+    const timerInterval = setInterval(() => {
+      if (!messageChanged){
+          setTimerValue((prevValue) => prevValue + interval / 1000);
+      }
+    }, interval);
+
+    return () => {
+      clearTimeout(timer);
+      clearInterval(timerInterval);
+    };
+  }, [messageChanged]);
+
+  return (
+    <div>
+      <p>Message: {message}</p>
+      <p>Timer: {timerValue.toFixed(2)} seconds</p>
+    </div>
+  );
+};
+
+export default Challenge3;
