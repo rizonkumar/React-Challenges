ed88cbe83be64b6960e46cc5f91d4c23f569dc33
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-02-07 10:42:08 +0530

    Completed Challenge -2

--- challenge-2/src/WithoutUsingUseState.js
+++ challenge-2/src/WithoutUsingUseState.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, {useRef, useState} from "react";
+
+// for doing this we will use "useRef" but that is not the idea way of solving it as it does not render the component
+// hence we are not able to render it we are forcely rendering the component
 
 const WithoutUsingUseState = () => {
-  return <div>WithoutUsingUseState</div>;
+
+  const counterRef = useRef(0);
+  const [, setForceUpdate] = useState(false);
+
+  const handleClick = () => {
+    counterRef.current++;
+
+    // force render (not recommended)
+    forceRender();
+  };
+
+  const forceRender = () => {
+    // Update a dummy state variable to trigger a re-render
+    // prev is the previous state of the state variable,
+    setForceUpdate((prev) => !prev);
+  }
+
+
+  return (
+  <div>
+    <p>Using a ref with forced re-rendering</p>
+    <button onClick={handleClick}>You pressed me {counterRef.current} times</button>
+  </div>
+  );
 };
 
 export default WithoutUsingUseState;
