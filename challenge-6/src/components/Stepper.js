da76f624bc049c618b5086db9d0fdc2823436a28
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-03-03 15:30:27 +0530

    inital commit

---
+++ challenge-6/src/components/Stepper.js
@@ -0,0 +1,19 @@
+const Stepper = ({ stepsConfig = [] }) => {
+  if (!stepsConfig.length) {
+    return <></>;
+  }
+  return (
+    <div className="stepper">
+      {stepsConfig.map((step, index) => {
+        return (
+          <div key={step.name} className="step">
+            <div className="step-number">{index + 1}</div>
+            <div className="step-name">{step.name}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Stepper;
