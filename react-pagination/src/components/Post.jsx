b3cb224f2fdd494d530194fea949c6c0e74645f5
rizon.rahi <rizon.rahi@ugamsolutions.com>
2024-07-15 11:07:35 +0530

    Feat: Error Handling

--- react-pagination/src/components/Post.jsx
+++ react-pagination/src/components/Post.jsx
@@ -5,21 +5,35 @@ import axios from "axios";
 const Post = () => {
   const [data, setData] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(`https://picsum.photos/v2/list?page=${pageNo}&limit=5`)
-      .then((res) => setData(res.data));
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://picsum.photos/v2/list?page=${pageNo}&limit=5`
+        );
+        setData(response.data);
+      } catch (error) {
+        setError("Error fetching data. Please try again later");
+      }
+    };
   }, [pageNo]);
 
   return (
     <div className="container">
-      <div className="post-container">
-        {data.map((item, index) => {
-          return <img src={item.download_url} key={index} />;
-        })}
-      </div>
-      <Pagination pageNo={pageNo} setPageNo={setPageNo} />
+      {error ? (
+        <div className="error-message">{error}</div>
+      ) : (
+        <>
+          <div className="post-container">
+            {data.map((item, index) => {
+              return <img src={item.download_url} key={index} />;
+            })}
+          </div>
+          <Pagination pageNo={pageNo} setPageNo={setPageNo} />
+        </>
+      )}
     </div>
   );
 };
