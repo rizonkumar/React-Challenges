6c6479dc33b7006480c8052cd4722b082dbd72f7
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-08-02 20:23:08 +0530

    Fixed: Rendering issue

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { List } from "./List";
+import "./Todo.css";
 
 export const Todo = () => {
   const [task, setTask] = useState();
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos') || []));
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos") || [])
+  );
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -28,52 +31,65 @@ export const Todo = () => {
     }
   };
 
-  const deleteToDo = (id) => {
-    const filteredToDo = todos.filter((todo) => {
-      return todo.id !== id;
+  const deleteToDo = useCallback((id) => {
+    setTodos((prevTodos) => {
+      const filteredToDo = prevTodos.filter((todo) => {
+        return todo.id !== id;
+      });
+      return filteredToDo;
     });
-    setTodos(filteredToDo);
-  };
+  }, []);
 
-  const completeTodo = (id) => {
-    const newTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
-    );
-    setTodos(newTodos);
-  };
+  const completeTodo = useCallback((id) => {
+    setTodos((prevTodos) => {
+      const newTodos = prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      );
+      return newTodos;
+    });
+  }, []);
 
-  const updateToDo = (id, updatedVal) => {
-    const newTodos = todos.map((todo) => {
-      if(id === todo.id) {
-        return {...todo, value: updatedVal}
-      } else {
-        return todo;
-      }
-    })
-    setTodos(newTodos);
-  }
+  const updateToDo = useCallback((id, updatedVal) => {
+    setTodos((prevTodos) => {
+      const newTodos = prevTodos.map((todo) => {
+        if (id === todo.id) {
+          return { ...todo, value: updatedVal };
+        } else {
+          return todo;
+        }
+      });
+      return newTodos;
+    });
+  }, []);
+
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
   return (
-    <div>
-      <div>
+    <div className="todo-container">
+      <h1>Todo List</h1>
+      <div className="input-container">
         <input
           type="text"
           value={task}
           onChange={handleChange}
           onKeyDown={handleKeyDown}
+          placeholder="Enter a new task"
         />
         <button onClick={addTodo}>Add Task</button>
       </div>
-      <div>
-        {todos.map((todo, index) => {
-          return (
-            <List key={todo.id} todo={todo} completeTodo={completeTodo} deleteToDo={deleteToDo} updateToDo={updateToDo} />
-          );
-        })}
-      </div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <List
+            key={todo.id}
+            todo={todo}
+            completeTodo={completeTodo}
+            deleteToDo={deleteToDo}
+            updateToDo={updateToDo}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
